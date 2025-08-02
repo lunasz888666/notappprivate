e635c0dcc927b1eb27b6610b675c9fce70619f86
lunasz888666 <lunasz888666@gmail.com>
2025-08-03 01:52:04 +0800

    123456

--- android/app/src/main/java/com/rabbitmaid/notesapp/MainApplication.kt
+++ android/app/src/main/java/com/rabbitmaid/notesapp/MainApplication.kt
@@ -2,7 +2,7 @@ package com.rabbitmaid.notesapp
 
 import android.app.Application
 import android.content.res.Configuration
-
+import android.util.Log
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
@@ -12,67 +12,58 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-
-
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
-
-import android.app.Application;
-import com.vungle.ads.VungleAds;
-import com.vungle.ads.InitializationListener;
-import com.vungle.ads.VungleError;
-import android.util.Log;
-
-import com.facebook.react.PackageList
-
+import com.vungle.ads.VungleAds
+import com.vungle.ads.InitializationListener
+import com.vungle.ads.VungleError
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+    companion object {
+        const val APP_ID = "你的 Vungle APP_ID"
+        const val TAG = "VungleInit"
+    }
+
+    override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
         this,
         object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages
-            packages.add(VunglePackage())
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(MyReactNativePackage())
-            return packages
-          }
+            override fun getPackages(): List<ReactPackage> {
+                val packages = PackageList(this).packages
+                packages.add(VunglePackage())
+                return packages
+            }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+            override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
+    )
 
-          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+    override val reactHost: ReactHost
+        get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
 
-          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-  )
+    override fun onCreate() {
+        super.onCreate()
+        VungleAds.init(this, APP_ID, object : InitializationListener {
+            override fun onSuccess() {
+                Log.d(TAG, "Vungle SDK init onSuccess()")
+            }
+            override fun onError(vungleError: VungleError) {
+                Log.d(TAG, "onError(): ${vungleError.errorMessage}")
+            }
+        })
 
-  override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-
-    VungleAds.init(requireContext(), APP_ID, object : InitializationListener {
-      override fun onSuccess() {
-        Log.d(TAG, "Vungle SDK init onSuccess()")
-      }
-      override fun onError(vungleError: VungleError) {
-        Log.d(TAG, "onError(): ${vungleError.localizedMessage}")
-      }
-    })
-
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+        SoLoader.init(this, OpenSourceMergedSoMapping)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            load()
+        }
+        ApplicationLifecycleDispatcher.onApplicationCreate(this)
     }
-    ApplicationLifecycleDispatcher.onApplicationCreate(this)
-  }
 
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
-  }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+    }
 }
